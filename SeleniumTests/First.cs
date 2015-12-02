77718c03f5402a0728c1922a0cc16dfb316b0c83
Alexander Kulakov <sanekurai@gmail.com>
2015-12-02 08:51:47 +0500

    add project

---
+++ SeleniumTests/First.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class First
+    {
+        private IWebDriver _webDriver;
+
+        //[TestCleanup]
+        //public void TestCleanup()
+        [TearDown]
+        public void RunAfterAnyTests()
+        { 
+            _webDriver.Quit();
+        }
+
+        //[TestMethod]
+        [Test]
+        public void TestMethod1()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Navigate().GoToUrl("http://google.com");
+            Assert.AreEqual("Fake Google", _webDriver.Title);
+        }
+    }
+}
