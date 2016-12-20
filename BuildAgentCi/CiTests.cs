8867aa0844785288d495a28acb5d73ca5507c74b
Alexander Kulakov <sanekurai@gmail.com>
2016-12-20 09:31:06 +0500

    add ci proj and yml

---
+++ BuildAgentCi/CiTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Assert = NUnit.Framework.Assert;
+
+namespace BuildAgentCi
+{
+    [TestFixture]
+    public class CiTests
+    {
+        private IWebDriver _webDriver;
+
+        [TearDown]
+        public void RunAfterAnyTests()
+        {
+            _webDriver.Quit();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Navigate().GoToUrl("http://google.com");
+            Assert.AreEqual("Fake Google", _webDriver.Title);
+        }
+    }
+}
